36471427c1545a15c04dafef65f0175b9097b101
Bazza <barry@bazzaware.co.uk>
2022-05-23 19:31:06 +0100

    Loaded All three data sets

--- net50/module3/before/Extensibility/PeopleViewer/Controllers/PeopleController.cs
+++ net50/module3/before/Extensibility/PeopleViewer/Controllers/PeopleController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PersonReader.CSV;
 using PersonReader.Interface;
+using PersonReader.ServiceReader;
+using PersonReader.SQL;
 using System.Collections.Generic;
 
 namespace PeopleViewer.Controllers
@@ -8,20 +11,32 @@ namespace PeopleViewer.Controllers
     {
         public IActionResult UseService()
         {
+            ServiceReader  reader = new ServiceReader();
+            IEnumerable<Person> person = reader.GetPeople();
+
+            ViewData["ReaderType"] = reader.GetType().ToString();
             ViewData["Title"] = "Using a Web Service";
-            return View("Index", new List<Person>());
+            return View("Index", person);
         }
 
         public IActionResult UseCSV()
         {
+            CSVReader reader = new CSVReader();
+            IEnumerable<Person> person = reader.GetPeople();
+
+            ViewData["ReaderType"] = reader.GetType().ToString();
             ViewData["Title"] = "Using a CSV File";
-            return View("Index", new List<Person>());
+            return View("Index", person);
         }
 
         public IActionResult UseSQL()
         {
+            SQLReader reader = new SQLReader();
+            IEnumerable<Person> person = reader.GetPeople();
+
+            ViewData["ReaderType"] = reader.GetType().ToString();
             ViewData["Title"] = "Using a SQL Database";
-            return View("Index", new List<Person>());
+            return View("Index", person);
         }
     }
 }
